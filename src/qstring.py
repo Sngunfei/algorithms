f3b3ea521b35a8021a7cb623ec3a94f41a98bca9
syfnico <syfnico@foxmail.com>
2019-10-21 20:16:45 +0800

    kmp

---
+++ src/qstring.py
@@ -0,0 +1,20 @@
+# -*- encoding:utf-8 -*-
+
+"""
+常用的字符串算法
+"""
+
+def kmp():
+    """
+    KMP算法
+    :return:
+    """
+    pass
+
+
+def reverse_order():
+    """
+    单词逆序，"abc def" => "cba fed"
+    :return:
+    """
+    pass
