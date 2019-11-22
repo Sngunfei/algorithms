03556d597fea6d56a882b61d6df06371cef447aa
syfnico <syfnico@foxmail.com>
2019-11-23 01:03:35 +0800

    Trie tree

---
+++ src/trieTree.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+"""
+字典树
+"""
+
+
+# todo
+class Trie:
+
+    def __init__(self):
+
+        raise NotImplementedError()
+
+    def insert_word(self, word: str) -> bool:
+
+        raise NotImplementedError()
+
+    def find_word(self, word: str) -> bool:
+
+        raise NotImplementedError()
+
+    def words_cnt(self) -> dict:
+
+        raise NotImplementedError()
+
